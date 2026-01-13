c27d657651a330e1eb179d16a3efff83a7e816de
GPCSantosh <santoshnithin7@gmail.com>
2026-01-13 12:19:19 +0530

    AI Career Prediction System with Salary Range

---
+++ frontend/script.js
@@ -0,0 +1,37 @@
+function predictCareer() {
+    const domain = document.getElementById("domain").value;
+    const experience = document.getElementById("experience").value;
+    const employment = document.getElementById("employment").value;
+    const company_size = document.getElementById("company_size").value;
+
+    if (!domain || !experience || !employment || !company_size) {
+        document.getElementById("result").innerText =
+            "Please select all fields.";
+        return;
+    }
+
+    fetch("http://127.0.0.1:8000/predict", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            domain,
+            experience,
+            employment,
+            company_size
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            document.getElementById("result").innerText = data.error;
+        } else {
+            document.getElementById("result").innerHTML =
+                `Predicted Role: <b>${data.predicted_role}</b><br>
+                 Expected Salary Range: <b>${data.salary_range_usd}</b>`;
+        }
+    })
+    .catch(() => {
+        document.getElementById("result").innerText =
+            "Backend connection failed";
+    });
+}
